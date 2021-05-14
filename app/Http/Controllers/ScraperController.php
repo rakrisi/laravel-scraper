618abd239af4de7908afa350f2438130998eb4a4
rakrisi1 <radhesingh01210@gmail.com>
2021-05-14 18:48:14 +0530

    done

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -12,7 +12,22 @@ class ScraperController extends Controller
     public function scraper()
     {
         $client = new Client();
-        $url = 'https://www.worldometers.info/coronavirus/';
+        // $url = 'https://www.worldometers.info/coronavirus/';
+        // $page = $client->request('GET', $url);
+
+        // /*echo "<pre>";
+        // print_r($page);*/
+
+        // // echo $page->filter('.maincounter-number')->text();
+
+        // $page->filter('#maincounter-wrap')->each(function ($item) {
+        //     $this->results[$item->filter('h1')->text()] = $item->filter('.maincounter-number')->text();
+        // });
+
+        // $data = $this->results;
+        // //return view('scraper', compact('data'));
+        // var_dump($data);
+        $url = 'https://www.menuwithprice.com/nutrition/';
         $page = $client->request('GET', $url);
 
         /*echo "<pre>";
@@ -20,11 +35,12 @@ class ScraperController extends Controller
 
         // echo $page->filter('.maincounter-number')->text();
 
-        $page->filter('#maincounter-wrap')->each(function ($item) {
-            $this->results[$item->filter('h1')->text()] = $item->filter('.maincounter-number')->text();
+        $page->filter('li > a')->each(function ($item) {
+            $this->results[$item->attr('href')] = $item->filter('a')->text();
         });
 
         $data = $this->results;
-        return view('scraper', compact('data'));
+        //return view('scraper', compact('data'));
+        dd($data);
     }
 }
